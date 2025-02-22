34e7a71857c6e8bd1a823abb26f7902257e8ff52
kaoyvtou <givsgoodusanameyt@gmail.com>
2025-02-22 12:03:43 -0500

    222

--- Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -1,24 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    string String = "Hello";
-    int IntegerA = 727;
+    Rigidbody2D rb;
+    public float speed = 3;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        string String2 = " World";
-        Debug.Log("Hello World");
-        Debug.Log(String + String2);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(IntegerA);
-        IntegerA++;
+
+        //
+        /*
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+       
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, rb.velocity.y);
+        }
+        */
+
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+    }
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+        switch (collision.tag)
+        {
+            case "Win":
+                {
+                    break;
+                }
+            case "Lose":
+                {
+                    break;
+                }
+            case "Death":
+                {
+                    string LV1 = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene( LV1 );
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene("LV1");
+                    break;
+                }
+        }
     }
 }
