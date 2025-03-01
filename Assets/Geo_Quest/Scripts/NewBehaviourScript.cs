eeddde3f91ede15c945dd5a6c54504fdcdb8525a
kaoyvtou <givsgoodusanameyt@gmail.com>
2025-03-01 11:12:49 -0500

    ---

--- Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -19,40 +19,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //
-        /*
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1, 0);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-       
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-1, rb.velocity.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.velocity = new Vector2(1, rb.velocity.y);
-        }
-        */
-
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
@@ -78,6 +44,10 @@ public class NewBehaviourScript : MonoBehaviour
             case "Finish":
                 {
                     SceneManager.LoadScene("LV1");
+                    break;
+                }
+            case "Teleport":
+                {
                     break;
                 }
         }
